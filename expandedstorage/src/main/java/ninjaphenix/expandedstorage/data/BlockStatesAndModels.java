52298baab80016cc93b2f4f48fb58b7aca086ac3
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-06-26 03:17:13 +0100

    Block model and block state data generation, needs item models.

--- expandedstorage/src/main/java/ninjaphenix/expandedstorage/data/BlockStatesAndModels.java
+++ expandedstorage/src/main/java/ninjaphenix/expandedstorage/data/BlockStatesAndModels.java
@@ -1,21 +1,20 @@
 package ninjaphenix.expandedstorage.data;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ConfiguredModel;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
-import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.*;
 import ninjaphenix.expandedstorage.ModContent;
+import ninjaphenix.expandedstorage.api.block.AbstractChestBlock;
 import ninjaphenix.expandedstorage.api.block.CursedChestBlock;
+import ninjaphenix.expandedstorage.api.block.OldChestBlock;
+import ninjaphenix.expandedstorage.api.block.enums.CursedChestType;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Function;
 
 public class BlockStatesAndModels extends BlockStateProvider
 {
+    private ModelFile OLD_CHEST_HORIZONTAL;
+    private ModelFile OLD_CHEST_VERTICAL;
+
     public BlockStatesAndModels(final DataGenerator generator, final String modId, final ExistingFileHelper fileHelper)
     {
         super(generator, modId, fileHelper);
@@ -24,17 +23,128 @@ public class BlockStatesAndModels extends BlockStateProvider
     @Override
     protected void registerStatesAndModels()
     {
-        //chestBlock(ModContent.WOOD_CHEST.getFirst(), bs -> null);
+        chestBlock(ModContent.WOOD_CHEST.getFirst());
+        chestBlock(ModContent.PUMPKIN_CHEST.getFirst());
+        chestBlock(ModContent.CHRISTMAS_CHEST.getFirst());
+        chestBlock(ModContent.IRON_CHEST.getFirst());
+        chestBlock(ModContent.GOLD_CHEST.getFirst());
+        chestBlock(ModContent.DIAMOND_CHEST.getFirst());
+        chestBlock(ModContent.OBSIDIAN_CHEST.getFirst());
+        OLD_CHEST_HORIZONTAL = models().getBuilder("block/old_chest/horizontal")
+                                       .parent(models().getExistingFile(mcLoc("cube")))
+                                       .texture("particle", "#front")
+                                       .texture("down", "#top")
+                                       .texture("up", "#top")
+                                       .texture("north", "#front")
+                                       .texture("east", "#left")
+                                       .texture("south", "#back")
+                                       .texture("west", "#right");
+        OLD_CHEST_VERTICAL = models().getBuilder("block/old_chest/vertical")
+                                     .parent(OLD_CHEST_HORIZONTAL)
+                                     .texture("east", "#side")
+                                     .texture("west", "#side")
+                                     .texture("south", "#side");
+
+        oldChestBlock(ModContent.OLD_WOOD_CHEST.getFirst());
+        oldChestBlock(ModContent.OLD_IRON_CHEST.getFirst());
+        oldChestBlock(ModContent.OLD_GOLD_CHEST.getFirst());
+        oldChestBlock(ModContent.OLD_DIAMOND_CHEST.getFirst());
+        oldChestBlock(ModContent.OLD_OBSIDIAN_CHEST.getFirst());
     }
 
     @Override
-    public String getName() { return "Expanded Storage - BlockStates / Models"; }
+    public @NotNull String getName() { return "Expanded Storage - BlockStates / Models"; }
 
-    private void chestBlock(final CursedChestBlock block, final Function<BlockState, ModelFile> modelFunc) {
-        getVariantBuilder(block).forAllStates(state ->
-                        ConfiguredModel.builder()
-                                       .modelFile(modelFunc.apply(state))
-                                       .build()
-        );
+    private void oldChestBlock(final OldChestBlock block)
+    {
+        getVariantBuilder(block).forAllStatesExcept(state -> {
+            final String blockPath = block.getRegistryName().getPath();
+            final CursedChestType chestType = state.get(AbstractChestBlock.TYPE);
+            return ConfiguredModel
+                    .builder()
+                    .rotationY(((state.get(BlockStateProperties.HORIZONTAL_FACING).getHorizontalIndex() + 2) % 4) * 90)
+                    .modelFile(oldChestGetModel(blockPath, chestType))
+                    .build();
+        }, BlockStateProperties.WATERLOGGED);
+    }
+
+    private ModelFile oldChestGetModel(final String blockPath, final CursedChestType chestType)
+    {
+        final String chestTypeName = chestType.getName();
+        final BlockModelBuilder builder = models().getBuilder(String.format("block/%s/%s", blockPath, chestTypeName));
+        switch (chestType)
+        {
+            case TOP:
+                builder.parent(OLD_CHEST_VERTICAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top", blockPath)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_%s", blockPath, chestTypeName)))
+                       .texture("side", modLoc(String.format("block/%s/chest_side_top", blockPath)));
+                break;
+            case BOTTOM:
+                builder.parent(OLD_CHEST_VERTICAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top", blockPath)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_%s", blockPath, chestTypeName)))
+                       .texture("side", modLoc(String.format("block/%s/chest_front_bottom", blockPath)));
+                break;
+            case SINGLE:
+                builder.parent(OLD_CHEST_VERTICAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top", blockPath)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_%s", blockPath, chestTypeName)))
+                       .texture("side", modLoc(String.format("block/%s/chest_side", blockPath)));
+                break;
+            case FRONT:
+                builder.parent(OLD_CHEST_HORIZONTAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top_%s", blockPath, chestTypeName)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_single", blockPath)))
+                       .texture("back", modLoc(String.format("block/%s/chest_side", blockPath)))
+                       .texture("left", modLoc(String.format("block/%s/chest_back_left", blockPath)))
+                       .texture("right", modLoc(String.format("block/%s/chest_back_right", blockPath)));
+                break;
+            case BACK:
+                builder.parent(OLD_CHEST_HORIZONTAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top_%s", blockPath, chestTypeName)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_single", blockPath)))
+                       .texture("back", modLoc(String.format("block/%s/chest_side", blockPath)))
+                       .texture("left", modLoc(String.format("block/%s/chest_back_right", blockPath)))
+                       .texture("right", modLoc(String.format("block/%s/chest_back_left", blockPath)));
+                break;
+            case LEFT:
+            case RIGHT:
+                builder.parent(OLD_CHEST_HORIZONTAL)
+                       .texture("top", modLoc(String.format("block/%s/chest_top_%s", blockPath, chestTypeName)))
+                       .texture("front", modLoc(String.format("block/%s/chest_front_%s", blockPath, chestTypeName)))
+                       .texture("back", modLoc(String.format("block/%s/chest_back_%s", blockPath, chestTypeName)))
+                       .texture("left", modLoc(String.format("block/%s/chest_side", blockPath)))
+                       .texture("right", modLoc(String.format("block/%s/chest_side", blockPath)));
+                break;
+        }
+        return builder;
+    }
+
+    private void chestBlock(final CursedChestBlock block)
+    {
+        getVariantBuilder(block).forAllStatesExcept(state -> ConfiguredModel
+                        .builder().modelFile(chestGetParticleTexture(state.get(AbstractChestBlock.TYPE), block.getRegistryName().getPath())).build(),
+                BlockStateProperties.WATERLOGGED, BlockStateProperties.HORIZONTAL_FACING);
+    }
+
+    private ModelFile chestGetParticleTexture(final CursedChestType type, final String blockPath)
+    {
+        final String chestName = blockPath.substring(0, blockPath.indexOf('_'));
+        if (blockPath.equals("pumpkin_chest"))
+        {
+            return models().getBuilder(blockPath).texture("particle", modLoc(String.format("block/%s_break", chestName)));
+        }
+        else if (blockPath.equals("christmas_chest"))
+        {
+            final String chestType = CursedChestType.TOP == type || CursedChestType.BOTTOM == type ? "tall" :
+                    CursedChestType.LEFT == type || CursedChestType.RIGHT == type ? "vanilla" :
+                            CursedChestType.FRONT == type || CursedChestType.BACK == type ? "long" : "single";
+            return models().getBuilder(chestType + "_" + blockPath).texture("particle", modLoc(String.format("block/%s_%s_break", chestType, chestName)));
+        }
+        else
+        {
+            return models().getBuilder(blockPath).texture("particle", modLoc("block/old_" + blockPath + "/chest_front_single"));
+        }
     }
 }
--- expandedstorage/src/main/java/ninjaphenix/expandedstorage/data/DataGenerators.java
+++ expandedstorage/src/main/java/ninjaphenix/expandedstorage/data/DataGenerators.java
@@ -1,6 +1,7 @@
 package ninjaphenix.expandedstorage.data;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -10,13 +11,15 @@ import ninjaphenix.expandedstorage.ExpandedStorage;
 public class DataGenerators
 {
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event)
+    public static void gatherData(final GatherDataEvent event)
     {
         final DataGenerator generator = event.getGenerator();
+        final ExistingFileHelper fileHelper = event.getExistingFileHelper();
         generator.addProvider(new BlockTags(generator));
         generator.addProvider(new ItemTags(generator));
         generator.addProvider(new Recipes(generator));
         generator.addProvider(new LootTables(generator));
         generator.addProvider(new Language(generator, ExpandedStorage.MOD_ID, "en_us"));
+        generator.addProvider(new BlockStatesAndModels(generator, ExpandedStorage.MOD_ID, fileHelper));
     }
 }
