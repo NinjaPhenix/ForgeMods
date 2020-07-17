d351fd0fd5b408547770a8f3e73c3ff3d2db320f
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-07-17 01:46:10 +0100

    Remove a completed todo.

--- torcherino/src/main/java/torcherino/block/package-info.java
+++ torcherino/src/main/java/torcherino/block/package-info.java
@@ -4,6 +4,3 @@ package torcherino.block;
 import mcp.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-// todo:
-//  Split common logic between all torcherino blocks into a separate class.
